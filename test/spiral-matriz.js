f199dd14cbb68774ef45f55809a90d6009f9384a
Guilherme Mangabeira Gregio <guilherme@3gweb.com.br>
2013-07-30 16:59:29 -0300

    created test to generate spiral matriz 5x1

--- test/spiral-matriz.js
+++ test/spiral-matriz.js
@@ -30,14 +30,24 @@ suite('Spiral Matriz', function () {
 	var spiral = require('../').spiral;
 
 	test('generate spiral matriz 1x1', function () {
-		expect(spiral(1,1)).to.eql([[1]]);
+		expect(spiral(1, 1)).to.eql([
+			[1]
+		]);
 	});
 
 	test('generate spiral matriz 1x5', function () {
-		expect(spiral(1,5)).to.eql([[1,2,3,4,5]]);
+		expect(spiral(1, 5)).to.eql([
+			[1, 2, 3, 4, 5]
+		]);
 	});
 
-	test('generate spiral matriz 1x5', function () {
-		expect(spiral(5,1)).to.eql([[1],[2],[3],[4],[5]]);
+	test('generate spiral matriz 5x1', function () {
+		expect(spiral(5, 1)).to.eql([
+			[1],
+			[2],
+			[3],
+			[4],
+			[5]
+		]);
 	});
 });
