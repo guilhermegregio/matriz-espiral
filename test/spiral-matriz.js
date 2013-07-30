68b0101f7257fd3c1f522f0640be979e04a93d05
Guilherme Mangabeira Gregio <guilherme@3gweb.com.br>
2013-07-30 15:53:31 -0300

    created test to return matriz 1x5

--- test/spiral-matriz.js
+++ test/spiral-matriz.js
@@ -4,8 +4,12 @@ var expect = require('chai').expect;
 suite('Spiral Matriz', function () {
 
 	var matriz = require('../').matriz;
-	
+
 	test('generate init matriz 1x1', function () {
 		expect(matriz(1,1)).to.eql([[0]]);
+	});
+
+	test('generate init matriz 1x5', function () {
+		expect(matriz(1,5)).to.eql([[0,0,0,0,0]]);
 	});
 });
